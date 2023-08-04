27a71d6afd66f3044c6784dbe0d14315795de480
satish <satish050801@gmail.com>
2023-08-04 16:12:04 +0530

    adding sidebar

---
+++ portfolio/script.js
@@ -0,0 +1,15 @@
+ 
+ const openButton =()=>{
+    const open = document.querySelector(' div.side-manu');
+    open.style.display = "inline-block";
+    const colsee = document.querySelector('div.side-menu-button');
+    open.style.display = "inline-block";
+    colsee.style.display = "none";
+    
+ }
+ const closeButton =()=>{
+    const open = document.querySelector(' div.side-manu');
+    open.style.display = "none";
+    const openn = document.querySelector(' div.side-menu-button');
+    openn.style.display = "inline-block";
+ }
